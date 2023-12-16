bed9ea4a356aa6cb70efaeb1d41f7c67748bc327
shira2299 <shiram33993@gmail.com>
2023-12-16 21:17:58 +0200

    Initial commit

---
+++ ToDoListReact/src/service.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+const apiUrl = "https://localhost:7241" 
+
+
+export default {
+  getTasks: async () => {
+    try{
+       const result = await axios.get(`${apiUrl}/items`)    
+       return result.data;
+    }catch(error){
+      console.error('Error in getTasks:', error);//throw error;
+    }
+   
+  },
+
+  addTask: async(name)=>{
+    console.log('addTask', name)
+    await axios.post(`${apiUrl}/items`,{name: name})  
+    return {};
+  },
+
+  setCompleted: async(id, isComplete)=>{
+    console.log('setCompleted', {id, isComplete})
+    const result = await axios.put(`${apiUrl}/items/${id}`, {isComplete: isComplete},{ headers: { 'Content-Type': 'application/json' }});
+    if(result.status === 200) {
+      return {success: true}
+    }else {
+      return {success: false, errorMessage: "Faild to update item."};
+    }
+  },
+
+  deleteTask:async(id)=>{
+    console.log('deleteTask',id)
+    await axios.delete(`${apiUrl}/items/${id}`) 
+      .catch(error => {
+        console.log('Error in deleteTask:',error);
+        throw error;// אם תרצי להמשיך לטפל בשגיאה במקום אחר, תוכלי לטפל בה כאן
+      })
+  }
+
+};
---
+++ TodoApi/Program.cs
@@ -0,0 +1,74 @@
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using TodoApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<ToDoDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("ToDoDB"), Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.31-mysql"))
+);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        }));
+
+
+var app = builder.Build();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+    });
+    app.UseSwagger(options =>
+    {
+    options.SerializeAsV2 = true;
+    });
+    app.UseCors("MyPolicy");
+
+app.MapGet("/items", async (ToDoDbContext dbContext) => {
+    var items = await dbContext.Items.ToListAsync();
+    // Debug
+    // printdebug("================");
+    return items;
+});
+
+app.MapGet("/items/{id}", async (int id, ToDoDbContext dbContext) => {
+    var item = await dbContext.Items.FindAsync(id);
+    return item;
+});
+
+app.MapPut("/items/{id}", async (int id, Item updateItem, ToDoDbContext dbContext) => {
+    var existItem = await dbContext.Items.FindAsync(id);
+    if(existItem is null) return Results.NotFound();
+
+    // existItem.Name = updateItem.Name;
+    existItem.IsComplete = updateItem.IsComplete;
+    await dbContext.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.MapPost("/items", async (Item item, ToDoDbContext dbContext) => {
+    dbContext.Items.Add(item);
+    await dbContext.SaveChangesAsync();
+    return dbContext.Items;
+    // return Results.Created("/items/${item.Id}", item);//מחזיר 201 יוצר אובייקט לאחר שהצליח החזיר 200
+});
+
+app.MapDelete("/items/{id}", async (ToDoDbContext dbContext, int id) => {   
+    var item = await dbContext.Items.FindAsync(id);
+    if(item!=null){
+        Console.WriteLine($"Deleting item: id:{item.Id} name:{item.Name} isComplete:{item.IsComplete}");
+        dbContext.Items.Remove(item);
+        await dbContext.SaveChangesAsync();
+    } 
+    return dbContext.Items;
+});
+
+app.Run();
